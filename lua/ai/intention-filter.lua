6537de5a56ca3fbd0181baaa7ceb0d825935eb0f
Hyper Cross <helios.tnak@gmail.com>
2011-07-01 01:19:53 -0400

    added new-ai's role guess matrix

--- lua/ai/intention-filter.lua
+++ lua/ai/intention-filter.lua
@@ -12,7 +12,7 @@ function SmartAI:intentionFilter(event,player,data)
 		if not info.from then 
 			info=data:toString() 
 			decisionType=decisionType or info:split(":")[1]
-			self:log(info)
+			--self:log(info)
 		end
 		
 		
@@ -24,10 +24,24 @@ function SmartAI:intentionFilter(event,player,data)
 		
 		
 	end
+	
+	if event==sgs.Death then
+		sgs.ai_transMX(sgs.role_mx,player,player:getRole(),10)
+	end
+	
+	-- if event==sgs.TurnStart then
+		-- for _,item in ipairs(sgs.ai_tracker) do
+			-- if item.match("turnStart",player,data) then
+				-- item.result(self,player,data)
+			-- end
+		-- end
+	-- end
 end
 
-function SmartAI:rememberToList(player,entry)
-	local name=player:objectName()
+function SmartAI:rememberToList(entry,player)
+	local name
+	if player then name=player:objectName()
+	else name="general" end
 	if not self.historian[name][entry.field] then 
 		self.historian[name][entry.field]={}
 	end
@@ -37,13 +51,18 @@ function SmartAI:rememberToList(player,entry)
 	end
 end
 
-function SmartAI:remember(player,entry)
-	local name=player:objectName()
+function SmartAI:remember(entry,player)
+	local name
+	if player then name=player:objectName()
+	else name="general" end
 	self.historian[name][entry.field]=entry.content
 end
 
-function SmartAI:recall(player,field)
-	return self.historian[player:objectName()][field]
+function SmartAI:recall(field,player)
+	local name
+	if player then name=player:objectName()
+	else name="general" end
+	return self.historian[name][field]
 end
 
  sgs.ai_tracker={}
@@ -55,7 +74,7 @@ sgs.newHistorian=function(player)
 		historian[aplayer:objectName()]={}
 
 	end
-	
+	historian["general"]={}
 	sgs.historians[player:objectName()]=historian
 	return historian
 end
@@ -74,8 +93,22 @@ function SmartAI:historianReport()
 
 	end
 	end
+	
+	if not sgs.role_mx then return end
+	for _,aplayer in sgs.qlist(self.room:getAlivePlayers()) do
+		
+			self:log(
+			math.ceil((sgs.role_mx.rebel[aplayer:objectName()] or 0)*100)/100 ..
+			" ; "..
+			math.ceil((sgs.role_mx.renegade[aplayer:objectName()] or 0)*100)/100 ..
+			" ; "..
+			math.ceil((sgs.role_mx.loyalist[aplayer:objectName()] or 0)*100)/100 ..
+			" : "..
+			aplayer:getGeneralName()
+			)
+		
+	end
 end
-
 
 sgs.newGuessing=function(spec)
 	local guessing={}
@@ -87,8 +120,8 @@ end
 
 sgs.ai_intention_carduse_mapping=
 {
-Indulgence="Disable",
-SupplyShortage="Disable",
+Indulgence="Jeopardize",
+SupplyShortage="Jeopardize",
 LeijiCard="Attack",
 RendeCard="Support",
 QingnangCard="Save",
@@ -102,8 +135,22 @@ QuhuCard="Jeopardize",
 JujianCard="Support",
 MingceCard="Support",
 XianzhenCard="Jeopardize",
+Slash="Attack",
+FireSlash="Attack",
+ThunderSlash="Attack",
+Peach="Save",
+Duel="Attack",
+Collateral="Disable",
+FireAttack="Attack",
+TuxiCard="Jeopardize",
+ShensuCard="Attack",
+QiangxiCard="Attack",
+QuhuCard="Jeopardize",
+JujianCard="Support",
+MingceCard="Support",
 }
 
+--cardUse
 sgs.newGuessing{
 	match=function(decisionType,player,data)
 		return decisionType=="cardUse"
@@ -112,11 +159,174 @@ sgs.newGuessing{
 		local info=data:toCardUse()
 		
 		for _,target in sgs.qlist(info.to) do
-			self:rememberToList(player,{field=target:objectName(),content=info.card:className()})
+			self:rememberToList({field=target:objectName(),content=info.card:className()},player)
 		end
 	end
 }
 
+--cardUse intention
+
+function sgs.interpreteMove(player,data)
+	local use=data:toCardUse()
+	local intention=sgs.ai_intention_carduse_mapping[use.card:className()]
+	return intention
+end
+
+sgs.newGuessing{
+	match=function(decisionType,player,data)
+		return decisionType=="cardUse"
+	end,
+	result=function(self,player,data)
+		if self~=sgs.recorder then return end
+		local use=data:toCardUse()
+		local intention=sgs.interpreteMove(player,data)
+		
+		if not sgs.role_mx then 
+			sgs.role_mx={}
+			local mx=sgs.role_mx
+			mx.rebel={}
+			mx.renegade={}
+			mx.loyalist={}
+			
+			local remains={}
+			for _,aplayer in sgs.qlist(player:getRoom():getAllPlayers()) do
+				if not aplayer:isLord() then 
+					remains[aplayer:getRole()]=(remains[aplayer:getRole()] or 0)+1
+				end
+			end
+			
+			for _,aplayer in sgs.qlist(player:getRoom():getAllPlayers()) do
+					
+				if not aplayer:isLord() then 	
+					mx.rebel[aplayer:objectName()]=remains.rebel
+					mx.loyalist[aplayer:objectName()]=remains.loyalist
+					mx.renegade[aplayer:objectName()]=remains.renegade
+				end
+			end
+			
+			
+		end
+		
+		if player:isLord() then return end
+			
+		local function transfer(mx,from,fr,to,tr,value)
+			mx[fr][from:objectName()]=mx[fr][from:objectName()]+value
+			mx[tr][to:objectName()]  =mx[tr][to:objectName()]  +value
+			mx[fr][to:objectName()]  =mx[fr][to:objectName()]  -value
+			mx[tr][from:objectName()]=mx[tr][from:objectName()]-value
+		end
+
+		local function findRoom(mx,from,fr,to,tr,inc)
+			local v
+			local sum=self.player_sum
+			if inc then
+				v=sum-mx[tr][to:objectName()]
+				if v>mx[tr][from:objectName()] then v=mx[tr][from:objectName()] end
+				if v>mx[fr][to:objectName()] then v=mx[fr][to:objectName()] end
+			else 
+				v=sum-mx[tr][to:objectName()]
+				if v<mx[tr][from:objectName()] then v=mx[tr][from:objectName()] end
+				if v<mx[fr][to:objectName()] then v=mx[fr][to:objectName()] end
+				v=sum-v
+			end
+			if v>0 then return v else return 0 end
+		end	
+			
+		local function transMX(mx,player,role,value)
+			local roles={"rebel","renegade","loyalist"}
+			local sum=self.player_sum
+			
+			if mx[role][player:objectName()]+value>sum then value=sum-mx[role][player:objectName()] end
+			if mx[role][player:objectName()]+value<0   then value=-mx[role][player:objectName()] end
+			
+			
+				local room=0
+				for _,arole in ipairs(roles) do
+					for _,aplayer in sgs.qlist(player:getRoom():getAlivePlayers()) do
+						if not aplayer:isLord() then
+							if not (arole==role or aplayer:objectName()==player:objectName()) then
+								room=room+findRoom(mx,player,role,aplayer,arole,value>0)
+							end
+							
+						end
+					end
+				end
+				
+				if room<=0 then return end
+				
+				for _,arole in ipairs(roles) do
+					for _,aplayer in sgs.qlist(player:getRoom():getAlivePlayers()) do
+						if not aplayer:isLord() then
+							if not (arole==role or aplayer:objectName()==player:objectName()) then
+								local transValue=findRoom(mx,player,role,aplayer,arole,value>0)*value/room
+								transfer(mx,player,role,aplayer,arole,transValue)
+							end
+							
+						end
+					end
+				end
+			
+			
+		end
+		
+		sgs.ai_transMX=transMX
+		
+		
+		
+		
+		for _,target in sgs.qlist(use.to) do
+			
+			if intention=="Attack" or intention=="Jeopardize" then
+				if target:isLord() then transMX(sgs.role_mx,player,"rebel",10) 
+				else
+				
+				local sum=self.player_sum
+				local v=sgs.role_mx.rebel[target:objectName()]-sum/2
+				if v>3 then v=3
+				elseif v<-1 then v=-1 end 
+				transMX(sgs.role_mx,player,"rebel",-v)
+				v=sgs.role_mx.loyalist[target:objectName()]-sum/2
+				if v>3 then v=3
+				elseif v<-1 then v=-1 end 
+				transMX(sgs.role_mx,player,"loyalist",-v)
+				
+				end
+			elseif intention=="Save" or intention=="Support" then 
+				if target:isLord() then transMX(sgs.role_mx,player,"rebel",-10) 
+				else
+				
+				local sum=self.player_sum
+				local v=sgs.role_mx.rebel[target:objectName()]-sum/2
+				if v>1 then v=1
+				elseif v<-3 then v=-3 end 
+				transMX(sgs.role_mx,player,"rebel",v)
+				v=sgs.role_mx.loyalist[target:objectName()]-sum/2
+				if v>1 then v=1
+				elseif v<-3 then v=-3 end 
+				transMX(sgs.role_mx,player,"loyalist",v)
+				
+				end
+			end
+			
+			
+		end
+		
+		local roles={"rebel","renegade","loyalist"}
+		local mostRole="rebel"
+		
+		for _,arole in ipairs(roles) do
+			if (not mostRole) or sgs.role_mx[mostRole][player:objectName()]< sgs.role_mx[arole][player:objectName()] then
+			mostRole=arole
+			end
+		end
+		
+		local mx=sgs.role_exp or {}
+		mx[player:objectName()]=mostRole
+		sgs.role_exp=mx
+	end,
+}
+
+--nullification
 sgs.newGuessing{
 	match=function(decisionType,player,data)
 		return decisionType=="cardUse" and data:toCardUse().card:inherits("Nullification")
@@ -131,10 +341,11 @@ sgs.newGuessing{
 		if source then text=text..source:objectName().."->" end
 		text=text..target:objectName()
 		
-		self:rememberToList(player,{field=target:objectName(),content=text.."_"..num})
+		self:rememberToList({field=target:objectName(),content=text.."_"..num},player)
 	end
 }
 
+--cardChosen
 sgs.newGuessing{
 	match=function(decisionType,player,data)
 		return decisionType=="cardChosen"
@@ -144,11 +355,12 @@ sgs.newGuessing{
 		local target=self.room:getCardOwner(info[3])
 		local place=self.room:getCardPlace(info[3])
 		
-			self:rememberToList(player,{field=target:objectName(),content=info[2].."="..info[3]..":"..place})
+			self:rememberToList({field=target:objectName(),content=info[2].."="..info[3]..":"..place},player)
 		
 	end
 }
 
+--peach
 sgs.newGuessing{
 	match=function(decisionType,player,data)
 		return decisionType=="peach"
@@ -157,27 +369,14 @@ sgs.newGuessing{
 		local info=data:toString():split(":")
 		local target=self:getPlayer(info[2])
 		
-			self:rememberToList(player,{field=target:objectName(),content="peach"})
+			self:rememberToList({field=target:objectName(),content="peach"},player)
 		
 	end
 }
 
+
 -- sgs.ai_intention_cardeffect_mapping={
--- Slash="Attack",
--- FireSlash="Attack",
--- ThunderSlash="Attack",
--- Peach="Save",
--- Duel="Attack",
--- Collateral="Disable",
--- FireAttack="Attack",
--- IronChain="Jeopardize",
--- TuxiCard="Jeopardize",
--- ShensuCard="Attack",
--- QiangxiCard="Attack",
--- QuhuCard="Jeopardize",
--- JujianCard="Support",
--- MingceCard="Support",
--- XianzhenCard="Jeopardize",
+
 -- }
 
 -- sgs.newGuessing{
