0f9425a4e730cc06bec7d434cd5fae0712dfcea3
Michael Johnson <504161487@qq.com>
2011-10-12 22:27:42 +0800

    fix some bugs in Guandu and Hongyan scenario.

--- src/scenario/guandu-scenario.h
+++ src/scenario/guandu-scenario.h
@@ -32,6 +32,7 @@ public:
     GuanduScenario();
 
     virtual AI::Relation relationTo(const ServerPlayer *a, const ServerPlayer *b) const;
+    virtual void getRoles(char *roles) const;
     virtual void onTagSet(Room *room, const QString &key) const;
 };
 
