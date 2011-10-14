9ab8d8f7d9b646081b0fed751b784badeb698839
Ubun Tenkei <ubun@foxmail.com>
2011-10-14 10:57:43 +0800

    Edited src/package/wisdom.h via GitHub

--- src/package/wisdom.h
+++ src/package/wisdom.h
@@ -11,7 +11,6 @@ class JuaoCard:public SkillCard{
 public:
     Q_INVOKABLE JuaoCard();
     virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    //virtual void use(Room *room, ServerPlayer *source, const QList<ServerPlayer *> &targets) const;
     virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
@@ -31,7 +30,6 @@ public:
     Q_INVOKABLE WeidaiCard();
 
     virtual void use(Room *room, ServerPlayer *source, const QList<ServerPlayer *> &targets) const;
-    virtual void onEffect(const CardEffectStruct &effect) const;
 };
 
 class HouyuanCard: public SkillCard{
