651b459840734191e76814386f0ddde4e8ede08b
Ubun Tenkei <ubun@foxmail.com>
2011-11-04 12:42:28 +0800

    lost "and"...

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -804,8 +804,8 @@ end
 
 -- yicai,badao,yitian-slash,moon-spear-slash
 sgs.ai_skill_use["slash"] = function(self, prompt)
-	if prompt ~= "@askforslash" prompt ~= "@moon-spear-slash" then return "." end
-    local slash = self:getCard("Slash")
+	if prompt ~= "@askforslash" and prompt ~= "@moon-spear-slash" then return "." end
+	local slash = self:getCard("Slash")
 	if not slash then return "." end
 	for _, enemy in ipairs(self.enemies) do
 		if self.player:canSlash(enemy, true) and not self:slashProhibit(slash, enemy) and self:slashIsEffective(slash, enemy) then
