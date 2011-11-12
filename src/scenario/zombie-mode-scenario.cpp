48048deda40dac497c6ffc29a1abb34e47e49e34
unknown <Hyper@.(none)>
2011-11-12 11:21:16 -0500

    fix a typo

--- src/scenario/zombie-mode-scenario.cpp
+++ src/scenario/zombie-mode-scenario.cpp
@@ -33,7 +33,7 @@ public:
 
         QString gender = player->getGeneral()->isMale() ? "male" : "female";
         room->broadcastInvoke("playAudio", QString("zombify-%1").arg(gender));
-        room->broadcastInvoke("updateStateItem", getRoleStateString());
+        room->broadcastInvoke("updateStateItem", room->getRoleStateString());
 
         player->tag.remove("zombie");
     }
