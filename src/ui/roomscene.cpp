0eb864272b4234c5953bd70623a209d585fd48f3
unknown <Hyper@.(none)>
2011-11-13 09:22:05 -0500

    corrected an syntax error

--- src/ui/roomscene.cpp
+++ src/ui/roomscene.cpp
@@ -572,8 +572,8 @@ QList<QPointF> RoomScene::getPhotoPositions() const{
 }
 
 void RoomScene::changeTextEditBackground(){
-    chat_box->setStyleSheet("background-color: rgba(117, 107, 97, 85%);");
-    log_box->setStyleSheet("background-color: rgba(97, 83, 77, 85%);");
+    chat_box->setStyleSheet("background-color: rgba(117, 107, 97, 90);");
+    log_box->setStyleSheet("background-color: rgba(97, 83, 77, 90);");
 }
 
 void RoomScene::addPlayer(ClientPlayer *player){
