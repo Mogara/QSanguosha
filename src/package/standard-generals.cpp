a7b520aaece9ceaacc8b87fccf6a92d2fbaf19aa
Moligaloo <moligaloo@gmail.com>
2011-12-10 16:07:03 +0800

    Fix the bug of Guicai don't invoke Moon Spear

--- src/package/standard-generals.cpp
+++ src/package/standard-generals.cpp
@@ -244,6 +244,7 @@ public:
 
     virtual const Card *viewAs(CardItem *card_item) const{
         Card *card = new GuicaiCard;
+        card->setSuit(card_item->getFilteredCard()->getSuit());
         card->addSubcard(card_item->getFilteredCard());
 
         return card;
