3ef99eb09228aafdae8da9558c10fb8f6a0ddf94
William <William915@gmail.com>
2011-12-31 19:35:54 +0800

    Fix a logical error in AI

--- lua/ai/wind-ai.lua
+++ lua/ai/wind-ai.lua
@@ -140,12 +140,10 @@ sgs.ai_skill_invoke["@guidao"]=function(self,prompt)
 
 	if #cards == 0 then return "." end
 	local card_id = self:getRetrialCardId(cards, judge)
-	if card_id == -1 then
-		if (self:isFriend(judge.who) and self:needRetrial(judge)) or (self:isEnemy(judge.who) and not self:needRetrial(judge)) then
-			self:sortByUseValue(cards, true)
-			if self:getUseValue(judge.card) > self:getUseValue(cards[1]) then
-				return "@GuidaoCard=" .. cards[1]:getId()
-			end
+	if card_id == -1 and self:needRetrial(judge) then
+		self:sortByUseValue(cards, true)
+		if self:getUseValue(judge.card) > self:getUseValue(cards[1]) then
+			return "@GuidaoCard=" .. cards[1]:getId()
 		end
 	elseif self:needRetrial(judge) or self:getUseValue(judge.card) > self:getUseValue(sgs.Sanguosha:getCard(card_id)) then
 		return "@GuidaoCard=" .. card_id
