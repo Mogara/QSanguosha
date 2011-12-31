1f58cf86b4ab4783e5cda893a5a6ca59dc6751cb
William <William915@gmail.com>
2011-12-31 23:50:34 +0800

    Modify the strategy of AI renegade

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -367,6 +367,11 @@ function SmartAI:objectiveLevel(player)
 			then return 3
 		else return 0 end
 	elseif self.role == "renegade" then
+		if SmartAI.GetValue(self.room:getLord()) < 6 and hasRebel then
+			if sgs.ai_loyalty[player:objectName()] < 0 then return 5
+			elseif player:isLord() then return -3
+			else return -1 end
+		end
 		local loyalish_hp, rebel_hp, loyalish_count, rebel_count = 0, 0
 		for _, aplayer in ipairs(players) do
 			if (sgs.ai_explicit[aplayer:objectName()] or ""):match("rebel") then
