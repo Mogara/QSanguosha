6b698786b48655916ee13002b95ff83dccf25f5d
William <William915@gmail.com>
2011-12-31 11:45:37 +0800

    Rewrite the role judging system of AI

--- lua/ai/wind-ai.lua
+++ lua/ai/wind-ai.lua
@@ -9,7 +9,7 @@ sgs.ai_skill_use["@@leiji"]=function(self,prompt)
 	self:updatePlayers()
 	self:sort(self.enemies,"hp")
 	for _,enemy in ipairs(self.enemies) do
-		if not self:isEquip("SilverLion", enemy) and not enemy:hasSkill("hongyan") then
+		if not self:isEquip("SilverLion", enemy) and not enemy:hasSkill("hongyan") and (sgs.ai_anti_lord[enemy:objectName()] or 0)>0 then
 			return "@LeijiCard=.->"..enemy:objectName()
 		end
 	end
