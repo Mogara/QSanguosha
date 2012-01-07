3104fc3e918d79e4c11a0726a961bd5ef7063ca4
hypercross <helios.tnak@gmail.com>
2012-01-07 14:40:18 -0500

    updated more extenson-docs

--- extension-doc/3-TriggerSkill.lua
+++ extension-doc/3-TriggerSkill.lua
@@ -1,22 +1,54 @@
 --技能详解3：TriggerSkill 触发技
 
---大部分技能都有”在xx时执行效果“这样的描述。任何非主动的技能也都可以用触发的方式来实现。
---也正因为如此，触发技种类繁多，也有很多不同的定义方式。
---基本的（也是万能的）触发技定义在Lua中是使用sgs.CreateViewAsSkill方法，该方法可以在lua\sgs_ex.lua中找到。
+--许多技能都有”在xx时机，如果xx条件满足，那么执行xx效果“这样的描述。
+--由于游戏技时机繁多，许多技能也都是相互类似的，我们在游戏的cpp部分有种类繁多的触发技定义方式。
+--而基本的（也是万能的）触发技定义在Lua中是使用sgs.CreateTriggerSkill方法，该方法可以在lua\sgs_ex.lua中找到。
 
---CreateViewAsSkill需要以下参数：
+--CreateTriggerSkill需要以下参数：
 
---name, frequency（可选）, events, on_trigger, can_trigger(可选)
+--name, frequency, events, on_trigger, can_trigger, priority
 
---其中name为技能名称字串
+--name：
+--技能名称字符串
+--没有默认……技能就是要有名字才行
 
---frequency是技能的发动频率，默认为不经常发动，也就是说在满足发动条件时询问发动。锁定技需要在这里将frequency设为compulsory强制发动。
+--frequency：
+--Frequency枚举类型，技能的发动频率。
+--执行askForSkillInvoke（询问技能发动）时，frequency会影响玩家做决定的方式。
+--frequency也起到了技能分类以及用于增加技能提示显示的作用。
+	--frequency可能的值有：
+	--sgs.Skill_Frequent（频繁发动：该技能会有一个可以打钩的按钮，如果勾选上，askForSkillInvoke就不会弹出提示而是直接返回true）
+	--sgs.Skill_NotFrequent（不频繁发动：该技能的askForSkillInvoke总是会弹出提示询问玩家是否发动）
+	--sgs.Skill_Compulsory （锁定技：该技能的默认优先度为2而不是1；该技能会在显示上提示玩家这是一个锁定技能）
+	--sgs.Skill_Limited （限定技：该技能会在显示上提示玩家这是一个限定技能）
+	--ssg.Skill_Wake（觉醒技：该技能的默认优先度为2而不是1；该技能会在显示上提示玩家这是一个觉醒技）
+--frequency的默认值为sgs.Skill_NotFrequent
 
---events为发动时机。具体的时机列表我会提供参考文档。events可以是lua table也可以是一个单独的events enum(本质上就是int)
+--events：
+--Event枚举类型，或者一个包含Event枚举类型的lua表。代表该技能的触发时机。
+--可用的Event列表请参考游戏代码中的struct.h文件。
+--无默认值。
 
---on_trigger为触发效果函数。对于具有多个触发时机的技能(如庸肆)，需要使用条件语句，分时机执行效果。
+--on_trigger:
+--lua函数，无返回值，执行事件触发时的技能效果。
+--如果需要区分不同的事件执行不同效果，请根据event参数使用条件语句。
+--通常需要将事件数据(data)转为具体的游戏结构对象才能进行操作。你可以在源码的swig/qvariant.i文件中看到定义。
+--on_trigger的传入参数为self(技能对象本身),event(当前触发的事件),player(事件触发者),data(事件数据)
+--无默认值。
 
---can_trigger为触发条件满足判断。对于满足发动条件的情况，此函数应该返回true。无定义时，默认为“具有此技能且存活”。
+--can_trigger:
+--lua函数，返回一个布尔值，即是否能触发该技能。
+--传入参数为self(技能对象),target(事件触发者)
+--默认条件为“具有本技能并且存活”
+--在这里个人只建议写简单的条件，许多判断都放在on_trigger里面做return其实都是可以的
+
+--priority:
+--整数值，代表本技能的优先度。
+--如果本技能与其他技能（或规则）在同一个时机都触发，那么优先度影响这些技能或规则的执行顺序。
+--优先度更大的技能（或规则）优先执行。游戏规则的优先度为0，典型的技能优先度为1，而受到伤害发动的技能优先度通常为-1.
+--锁定技和觉醒技的优先度默认为2，其他情况下默认为1
+
+-- **实例：
 
 --以下是曹操奸雄的实现：
 
@@ -31,6 +63,7 @@ jianxiong=sgs.CreateTriggerSkill{
 	on_trigger=function(self,event,player,data)
 		local room = player:getRoom()
 		local card = data:toDamage().card
+		--这两步通常是必要的。我们需要room对象来操作大多数的游戏要素，我们也需要将data对象转成对应的数据类型来得到相应的信息。
 		if not room:obtainable(card,player) then return end
 		if room:askForSkillInvoke(player,"jianxiong") then
 			room:playSkillEffect("jianxiong")
@@ -68,7 +101,7 @@ yongsi=sgs.CreateTriggerSkill{
 	
 		local room=player:getRoom()
 		
-		local getKingdoms=function() --可以在函数中定义函数
+		local getKingdoms=function() --可以在函数中定义函数，本函数返回存活势力的数目
 			local kingdoms={}
 			local kingdom_number=0
 			local players=room:getAlivePlayers()
@@ -82,14 +115,19 @@ yongsi=sgs.CreateTriggerSkill{
 		end
 		
 		if event==sgs.DrawNCards then 
+			--摸牌阶段，改变摸牌数
 			room:playSkillEffect("yongsi")
-			data:setValue(data:toInt()+getKingdoms()) --DrawNCards事件的data是一个int类型的QVariant，改变该QVariant对象会改变摸牌数
+			data:setValue(data:toInt()+getKingdoms()) 
+			--DrawNCards事件的data是一个int类型的QVariant即摸牌数，改变该QVariant对象会改变摸牌数
 		elseif (event==sgs.PhaseChange) and (player:getPhase()==sgs.Player_Discard) then
 			--进入弃牌阶段时，先执行庸肆弃牌，然后再执行常规弃牌
 			local x = getKingdoms()
 			local e = player:getEquips():length()+player:getHandcardNum()
-			if e>x then room:askForDiscard(player,"yongsi",x,false,true) -- 最后两个参数为”是否强制“以及”是否包含装备“
+			if e>x then room:askForDiscard(player,"yongsi",x,false,true) 
+			--要求玩家弃掉一些牌
+			-- 最后两个参数为”是否强制“以及”是否包含装备“
 			else 
+			--如果玩家的牌未达到庸肆的弃牌数目，那么跳过询问全部弃掉
 				player:throwAllHandCards()
 				player:throwAllEquips()
 			end
--- extension-doc/4-SkillCard.lua
+++ extension-doc/4-SkillCard.lua
@@ -1,6 +1,6 @@
 --技能讲解2：技能牌SkillCard
 
---神杀中，技能的效果在很多时候都技能牌实现。即，把技能定义在一张没有实体的抽象“牌”当中，当你发动技能的时候，视为你打出了这张牌。
+--神杀中，技能的效果在很多时候都技能牌实现。即，把技能定义在一张没有实体的抽象“牌”当中，当你发动技能的时候，视为你使用了这张牌。
 
 --对于指定对象发动的技能，对象的指定也算在牌的效果当中。
 --很多游戏的技能发动都带有cost这个概念，即发动技能的代价。神杀中，cost只能是你的牌或装备；也就是说，
@@ -9,26 +9,54 @@
 --由于技能牌的需要有多个实例存在（每次发动技能得到一个技能牌），
 --我们在DIY module当中并不像ViewAsSkill和TriggerSkill当中使用构造函数来创建SkillCard。
 --我们需要将SkillCard的参数在一个lua table当中定义好，然后在每次需要创建SkillCard的时候再调用sgs.CreateSkillCard获取SkillCard对象。
+--或者，我们也可以先创建好一个SkillCard，然后在技能中复制它。
 
 --sgs.CreateSkillCard需要以下参数定义：
 
 --name,target_fixed,will_throw, filter,feasible,on_use,on_effect
 
---target_fixed 和 will_throw为布尔量，代表技能是否需要指定对象发动，以及技能发动后该牌是否弃置。对于技能牌而言，即是说发动该技能所用的牌是否弃置。
---即使是以某名玩家为目标的技能，若总是没有选择（如陷阵杀），target_fixed也建议设为true。
---即使是大多数情况都没有选择的技能，若依然有可能有选择（如好施），target_fixed也建议设为false。
+--name:
+--字符串，牌的名字。取个好听的名字~
+--没有默认值。快去取名字……
 
+--target_fixed：
+--布尔值，使用该牌时是否需要玩家指定目标。
+--默认为false，使用时你需要指定目标，然后点确定。
+
+--will_throw:
+--布尔值，该牌在使用后是否被弃置。还记得subCards吗？
 --对于拼点技能，请将will_throw设为false，否则对方将看到你的牌之后再选择拼点牌。
+--也可以将will_throw设为false,然后使用room:throwCard(card)这个方法来灵活地控制如何将牌移动到弃牌区。
+--默认值为true。
 
---也可以使用room:throwCard(card)这个方法来将牌移动到弃牌区。
+--filter：
+--lua函数，返回一个布尔值，类似于ViewAsSkill中的view_filter，但filter方法的对象是玩家目标。
+--你在使用牌时只能指定玩家为对象，不能直接指定玩家的某张牌为对象；
+--比如过河拆桥，在神杀中，“选择对方的一张牌并弃置”是过河拆桥的效果，但过河拆桥的对象只有对方玩家。
+--如果你确实需要“作为对象的牌”，请还是在了解游戏机制后自行发明解决方法……
+--传入参数为self,targets(已经选择的玩家),to_select(需要判断是否能选择的玩家)
+--默认条件为“一名其他玩家”。
 
---filter方法与ViewAsSkill中的view_filter相似，但filter方法的对象是玩家目标。
---对于还需选择目标玩家的牌（比如装备）的效果，请理解为“选择并获取某玩家的一张牌”是你的牌的后续效果，并不是牌本身的目标。
-
---feasible方法则相当于viewasSkill的view_as方法中invalid_condition的作用。
+--feasible：
+--lua函数，返回一个布尔值，相当于viewasSkill的view_as方法是否应该返回nil。
 --在viewAsSkill中，我们可以无数次选中牌，直到返回了有意义的view_as再点确定，
---所以view_as返回了无意义的Nil也无所谓；然而在SkillCard当中，由于点确定的机会只有一次，我们必须要在实际的效果方法外排除无效的SkillCard的情况。
+--所以view_as返回了无意义的Nil也无所谓；然而在SkillCard当中，点确定的机会只有一次，
+--因此我们规定用feasible来排除无效使用的情况。
+--只有在feasible返回了true时，你才可以点确定。
+--传入参数为self,targets(已经选择的玩家)
+--默认条件为"target_fixed为true(不需要指定目标)，或者选择了至少一名玩家为目标"
 
+--on_use:
+--lua函数，无返回值，类似于on_trigger，执行使用效果。
+--传入参数为self,room(游戏房间对象),source(使用者),targets(牌的使用目标)
+--无默认。
+
+--on_effect：
+--lua函数，无返回值，同样用于执行使用效果，但只定义对于某一个目标的效果。
+--通常情况下你只需要写on_effect或者on_use当中的一个。
+--如果是没有目标或者是目标特定的技能，使用on_use；
+--如果是有几个目标执行相同或类似的效果，使用on_effect。
+--如果是玩家指定的目标，还是使用on_effect。
 
 --以下为“离间牌”的 feasible 以及filter方法：
 
@@ -45,7 +73,10 @@ feasible=function(self,targets)
 	--只有选择了2名玩家时，才能使用牌
 end
 
---on_use 和 on_effect 为牌的效果执行。两者通常是只需要实现一个的，他们的区别在于生效时机的不同。on_use在牌被打出时即生效，而on_effect在牌对某一名目标进行结算时生效。因此，在不存在需要结算的“一名玩家对另一名玩家”的效果时，使用on_use实行效果即可；而存在指定的目标时，则原则上应该使用on_effect。
+--on_use 和 on_effect 为牌的效果执行。他们的区别在于生效时机的不同。
+--on_use在牌被使用时生效，而on_effect在牌在使用中对某一名目标进行结算时生效。
+--因此，在不存在需要结算“一名玩家对另一名指定的玩家”的效果时，使用on_use实行效果即可；
+--存在指定的目标时，则原则上应该使用on_effect。
 
 --on_use和on_effect可以同时存在。牌效果进行结算时，先执行on_use，然后对每名目标执行on_effect
 
@@ -53,7 +84,7 @@ end
 
 on_effect=function(self,effect)
 
-	--effect 为一个CardEffectStruct，其from和to为player对象，代表谁打出的牌对谁结算
+	--effect 为一个CardEffectStruct，其from和to为player对象，代表谁使用的牌对谁结算
 	local from=effect.from
 	local to  =effect.to
 	local room=to:getRoom()
@@ -63,7 +94,9 @@ on_effect=function(self,effect)
 	
 	--进行判定时，首先创建“判定”对象。
 	--pattern为一个正则表达式，由冒号隔开的三段分别匹配牌名、花色和点数	
-	--good的定义和之后的判定结果获取有关。原则上，之前的pattern与判定牌匹配时，如果这种情况下执行的效果对于判定者来说是“好”的，那么good应该是true。
+	--good的定义和之后的判定结果获取有关。
+	--原则上，之前的pattern与判定牌匹配时，如果这种情况下执行的效果对于判定者来说是“好”的，
+	--那么good应该是true。
 	local judge=sgs.JudgeStruct()
 	judge.pattern=sgs.QRegExp("(.*):(spade):(.*)")
 	judge.good=false
@@ -93,10 +126,11 @@ end,
 --以下为孙权“制衡”的on_use方法：
 
 on_use=function(self,room,source,targets)
-	--self代表技能牌本身。由于是将“任意张牌当成制衡牌打出”，因此弃置制衡牌就等于弃置所有用来发动制衡的牌，也即被制衡掉的牌。
+	--self代表技能牌本身。由于是将“任意张牌当成制衡牌打出”，
+	--因此弃置制衡牌就等于弃置所有用来发动制衡的牌，也即被制衡掉的牌。
 	room:throwCard(self)	
 
 	--摸取相当于被用来发动制衡的牌的数目的牌。
 	--可以用self:getSubcards()来获取这些牌的QList。
-	room:drawCards(source,#self:getSubcards())
+	room:drawCards(source,self:getSubcards():length())
 end,
