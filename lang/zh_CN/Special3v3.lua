58f8533883f1ee66a3a78d170d66bdcda5069759
Moligaloo <moligaloo@gmail.com>
2012-03-07 15:40:39 +0800

    Add special 3v3 translation file

---
+++ lang/zh_CN/Special3v3.lua
@@ -0,0 +1,13 @@
+-- translation for Special3v3 Package
+
+return {
+	["Special3v3"] = "新3v3",
+	
+	["zhugejin"] = "诸葛瑾",
+	["hongyuan"] = "弘援",
+	[":hongyuan"] = "摸牌阶段摸牌时，你可以少摸一张牌，然后令其他己方角色各摸一张牌。",
+	["huanshi"] = "缓释",
+	[":huanshi"] = "在己方角色的判定牌生效前，你可以打出一张牌代替之。",
+	["mingzhe"] = "明哲",
+	[":mingzhe"] = "你的回合外，当你使用、打出或弃置一张红色牌时，你可以摸一张牌。",
+}
