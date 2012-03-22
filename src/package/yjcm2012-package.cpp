73bd23580045b378ac73e23b91a3c1b320b024f8
Moligaloo <moligaloo@gmail.com>
2012-03-22 19:48:44 +0800

    Add new property color for class Card

--- src/package/yjcm2012-package.cpp
+++ src/package/yjcm2012-package.cpp
@@ -5,11 +5,48 @@
 #include "carditem.h"
 #include "engine.h"
 
+class Zhiyu: public MasochismSkill{
+public:
+    Zhiyu():MasochismSkill("zhiyu"){
+
+    }
+
+    virtual void onDamaged(ServerPlayer *target, const DamageStruct &damage) const{
+        if(target->askForSkillInvoke(objectName(), QVariant::fromValue(damage))){
+            target->drawCards(1);
+
+            Room *room = target->getRoom();
+            room->showAllCards(target);
+
+            QList<const Card *> cards = target->getHandcards();
+            Card::Color color = cards.first()->getColor();
+            bool same_color = true;
+            foreach(const Card *card, cards){
+                if(card->getColor() != color){
+                    same_color = false;
+                    break;
+                }
+            }
+
+            if(same_color && damage.from){
+                DamageStruct zhiyu_damage;
+                zhiyu_damage.from = target;
+                zhiyu_damage.to = damage.from;
+
+                room->damage(zhiyu_damage);
+            }
+        }
+    }
+};
+
+
 YJCM2012Package::YJCM2012Package():Package("YJCM2012"){
+    General *xunyou = new General(this, "xunyou", "wei", 3);
+    xunyou->addSkill(new Zhiyu);
+
 /*
     General *wangyi = new General(this, "wangyi", "wei", 3, false);
 
-    General *xunyou = new General(this, "xunyou", "wei", 3);
 
     General *caozhang = new General(this, "caozhang", "wei");
 
