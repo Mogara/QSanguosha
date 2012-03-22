5340dc12a10575e476f7c5f3b679d9027d3438ee
Ubun <ubun@foxmail.com>
2012-03-22 19:51:03 +0800

    YJCM2012 - Madai

--- src/package/yjcm2012-package.cpp
+++ src/package/yjcm2012-package.cpp
@@ -5,6 +5,31 @@
 #include "carditem.h"
 #include "engine.h"
 
+class Fuji: public TriggerSkill{
+public:
+    Fuji():TriggerSkill("fuji"){
+        events << Predamage;
+        frequency = Compulsory;
+    }
+
+    virtual bool trigger(TriggerEvent , ServerPlayer *player, QVariant &data) const{
+        Room *room = player->getRoom();
+        DamageStruct damage = data.value<DamageStruct>();
+
+        if(player->distanceTo(damage.to) == 1 && damage.card && damage.card->inherits("Slash")){
+            player->getRoom()->playSkillEffect(objectName());
+            LogMessage log;
+            log.type = "#TriggerSkill";
+            log.from = player;
+            log.to << damage.to;
+            room->sendLog(log);
+            room->loseMaxHp(damage.to);
+            return true;
+        }
+        return false;
+    }
+};
+
 YJCM2012Package::YJCM2012Package():Package("YJCM2012"){
 /*
     General *wangyi = new General(this, "wangyi", "wei", 3, false);
@@ -12,9 +37,10 @@ YJCM2012Package::YJCM2012Package():Package("YJCM2012"){
     General *xunyou = new General(this, "xunyou", "wei", 3);
 
     General *caozhang = new General(this, "caozhang", "wei");
-
+*/
     General *madai = new General(this, "madai", "shu");
-
+    madai->addSkill(new Fuji);
+/*
     General *liaohua = new General(this, "liaohua", "shu");
 
     General *guanxingzhangbao = new General(this, "guanxingzhangbao", "shu");
