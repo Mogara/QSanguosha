bcbf6c35c6f4a0881b4c9eec46b45b595dfae7c0
clarkcyt <Clark.cyt@gmail.com>
2012-03-23 05:29:07 +0800

    Add general Bulianshi & Chengpu（Code by lycio）

--- src/package/yjcm2012-package.h
+++ src/package/yjcm2012-package.h
@@ -20,4 +20,24 @@ public:
     virtual void use(Room *room, ServerPlayer *source, const QList<ServerPlayer *> &targets) const;
 };
 
+class AnxuCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE AnxuCard();
+
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+    virtual void use(Room *room, ServerPlayer *source, const QList<ServerPlayer *> &targets) const;
+};
+
+class ChunlaoCard: public SkillCard{
+    Q_OBJECT
+
+public:
+    Q_INVOKABLE ChunlaoCard();
+
+    virtual void use(Room *room, ServerPlayer *source, const QList<ServerPlayer *> &targets) const;
+};
+
 #endif // YJCM2012PACKAGE_H
