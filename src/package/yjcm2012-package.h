e590d235559e42ea47fee953f22715a948a8a7f1
clarkcyt <Clark.cyt@gmail.com>
2012-03-23 14:09:20 +0800

    add general xunyou（code by lycio）

--- src/package/yjcm2012-package.h
+++ src/package/yjcm2012-package.h
@@ -3,10 +3,13 @@
 
 #include "package.h"
 #include "card.h"
-
+#include <QMutex>
 #include <QGroupBox>
 #include <QAbstractButton>
 #include <QButtonGroup>
+#include <QDialog>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QCommandLinkButton>
 #include <QLayout>
 #include <QDialog>
@@ -16,6 +19,39 @@ class YJCM2012Package: public Package{
 
 public:
     YJCM2012Package();
+};
+
+class QiceCard: public SkillCard{
+    Q_OBJECT
+
+
+public:
+    Q_INVOKABLE QiceCard();
+
+    virtual bool targetFixed() const;
+    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
+    virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+
+    virtual const Card *validate(const CardUseStruct *card_use) const;
+};
+
+class QiceDialog: public QDialog{
+    Q_OBJECT
+
+public:
+    static QiceDialog *GetInstance();
+
+public slots:
+    void popup();
+    void selectCard(QAbstractButton *button);
+
+private:
+    QiceDialog();
+
+    QGroupBox *createRight();
+    QAbstractButton *createButton(const Card *card);
+    QButtonGroup *group;
+    QHash<QString, const Card *> map;
 };
 
 class ZhenlieCard: public SkillCard{
