04d0dc0f6fb9090edd9dbc397a91bdc4c032cf03
Ubun <ubun@foxmail.com>
2012-03-24 15:34:56 +0800

    3v3 zhugejin's huanshi AI

---
+++ lua/ai/special3v3-ai.lua
@@ -0,0 +1,13 @@
+sgs.ai_skill_cardask["@huanshi-card"] = function(self, data)
+	local judge = data:toJudge()
+
+	if self:needRetrial(judge) then
+		local cards = sgs.QList2Table(self.player:getCards("he"))
+		local card_id = self:getRetrialCardId(cards, judge)
+		if card_id ~= -1 then
+			return "@HuanshiCard=" .. card_id
+		end
+	end
+
+	return "."
+end
