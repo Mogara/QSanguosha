ce294de01ee2646a83911da85a1e48453bcfb0ec
William <William915@gmail.com>
2012-03-28 12:56:54 +0800

    Fix issue #128

--- lua/ai/yitian-ai.lua
+++ lua/ai/yitian-ai.lua
@@ -26,15 +26,17 @@ local function findPlayerForModifyKingdom(self, players)
 	local isGood = self:isFriend(lord)
 
 	for _, player in sgs.qlist(players) do
-		if  sgs.evaluateRoleTrends(player) == "loyalist" then
-			local sameKingdom = player:getKingdom() == lord:getKingdom()
-			if isGood ~= sameKingdom then
-				return player
-			end
-		elseif lord:hasLordSkill("xueyi") and not player:isLord() then
-			local isQun = player:getKingdom() == "qun"
-			if isGood ~= isQun then
-				return player
+		if not player:isLord() or player:hasLordSkill("weidai") then
+			if  sgs.evaluateRoleTrends(player) == "loyalist" then
+				local sameKingdom = player:getKingdom() == lord:getKingdom() 
+				if isGood ~= sameKingdom then
+					return player
+				end
+			elseif lord:hasLordSkill("xueyi") and not player:isLord() then
+				local isQun = player:getKingdom() == "qun"
+				if isGood ~= isQun then
+					return player
+				end
 			end
 		end
 	end
