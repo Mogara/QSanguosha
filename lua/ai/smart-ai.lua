b7bcb878a5e2dc0b56692e6f011eddf04ae1f912
clarkcyt <Clark.cyt@gmail.com>
2012-04-05 21:17:07 +0800

    update ai for there are only lord and renegades

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -1187,7 +1187,8 @@ function SmartAI:objectiveLevel(player)
 				end
 			else
 				if player:isLord() then
-					if not sgs.isLordHealthy() then return 3
+					if sgs.isLordInDanger then return 0
+					elseif not sgs.isLordHealthy() then return 3
 					else return 5 end
 				elseif sgs.isLordHealthy() then return 3
 				else
