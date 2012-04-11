15e8bab55a3bea532336cb412b4b42ffc77847c6
yinlin09 <yinlin09@gmail.com>
2012-04-11 10:28:55 -0400

    Fixed a bug with huashen that could cause server crash

--- src/package/mountainpackage.cpp
+++ src/package/mountainpackage.cpp
@@ -1043,8 +1043,10 @@ public:
                     }
                 }
             }
-            skill_name = ai->askForChoice("huashen", skill_names.join("+"));
+            Q_ASSERT(skill_names.length() > 0);
+            skill_name = ai->askForChoice(skill_names.first(), skill_names.join("+"));
             const General* general = hash[skill_name];
+            Q_ASSERT(general != NULL);
             QString kingdom = general->getKingdom();
             if(zuoci->getKingdom() != kingdom){
                 if(kingdom == "god")
