4e7e1106e8ac503066ef4bdde377a1887b0271ab
Moligaloo <moligaloo@gmail.com>
2012-04-21 03:20:21 +0800

    Remove extra comma and semicolon

--- src/core/protocol.h
+++ src/core/protocol.h
@@ -83,7 +83,7 @@ namespace QSanProtocol
     enum Game3v3ChooseOrderCommand
     {
         S_REASON_CHOOSE_ORDER_TURN,
-        S_REASON_CHOOSE_ORDER_SELECT,
+        S_REASON_CHOOSE_ORDER_SELECT
     };
 
     enum Game3v3Camp
@@ -137,6 +137,6 @@ namespace QSanProtocol
 
         Json::Reader m_jsonReader;
     };    
-};
+}
 
 #endif
