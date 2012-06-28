d9afdc1b3bc195bb3880b2a7122360064e08e04a
clarkcyt <Clark.cyt@gmail.com>
2012-06-28 10:45:04 +0800

    fix a typo

--- src/package/bgm-package.cpp
+++ src/package/bgm-package.cpp
@@ -838,7 +838,7 @@ public:
             newdamage.transfer = true;
 
             room->damage(newdamage);
-            if(target->isAlive() && p->getMark("hate"+player->objectName()) > 0)
+            if(target->isAlive() && target->getMark("hate"+player->objectName()) > 0)
                 target->drawCards(damage.damage);
             return true;
         }
