228b7ce91567120fdf3842dd4bcc96fedbe6530a
Ellis <ellis_loveless@qq.com>
2012-07-04 11:11:04 +0800

    fix the image size in prompt box

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -837,12 +837,15 @@ void Client::askForNullification(const Json::Value &arg){
     }
 
     QString trick_path = G_ROOM_SKIN.getCardMainPixmapPath(trick_card->objectName());
+    QString arrow_src = "<img src='image/system/to.png' />";
     QString to = G_ROOM_SKIN.getGeneralPixmapPath(target_player->getGeneralName(), QSanRoomSkin::S_GENERAL_ICON_SIZE_LARGE);
+    QString to_src = QString("<img src='%1' width='96' height='96'/>").arg(to);
     if(source == NULL){
-        prompt_doc->setHtml(QString("<img src='%1' /> ==&gt; <img src='%2' />").arg(trick_path).arg(to));
+        prompt_doc->setHtml(QString("<img src='%1' /> %2 %3").arg(trick_path).arg(arrow_src).arg(to_src));
     }else{
         QString from = G_ROOM_SKIN.getGeneralPixmapPath(source->getGeneralName(), QSanRoomSkin::S_GENERAL_ICON_SIZE_LARGE);
-        prompt_doc->setHtml(QString("<img src='%1' /> <img src='%2'/> ==&gt; <img src='%3' />").arg(trick_path).arg(from).arg(to));
+        QString from_src = QString("<img src='%1' width='96' height='96'/>").arg(from);
+        prompt_doc->setHtml(QString("<img src='%1' /> %2 %3 %4").arg(trick_path).arg(from_src).arg(arrow_src).arg(to_src));
     }
 
     card_pattern = "nullification";
