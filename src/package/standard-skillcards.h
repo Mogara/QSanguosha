c78e79d26bd00fef7b1194a7fa506ee099ed6cb0
Ellis <ellis_loveless@qq.com>
2012-07-05 00:59:44 +0800

    fix a bug of lijian

--- src/package/standard-skillcards.h
+++ src/package/standard-skillcards.h
@@ -63,6 +63,7 @@ public:
 
     virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
     virtual bool targetsFeasible(const QList<const Player *> &targets, const Player *Self) const;
+    virtual void onUse(Room *room, const CardUseStruct &card_use) const;
     virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
 };
 
