15d61b29a9e4ea912fa2afb18ffe96131a174629
clarkcyt <Clark.cyt@gmail.com>
2012-07-15 09:53:55 +0800

    update the version number of ai

--- lua/ai/smart-ai.lua
+++ lua/ai/smart-ai.lua
@@ -10,7 +10,7 @@ math.randomseed(os.time())
 -- SmartAI is the base class for all other specialized AI classes
 SmartAI = class "SmartAI"
 
-version = "QSanguosha AI 20120405 (V0.8 Stable)"
+version = "QSanguosha AI 20120715 (V0.85 Stable)"
 --- this function is only function that exposed to the host program
 --- and it clones an AI instance by general name
 -- @param player The ServerPlayer object that want to create the AI object
