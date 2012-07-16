1370599d31c010be7fd8efe3669863d411372744
yinlin09 <yinlin09@gmail.com>
2012-07-16 01:00:07 -0700

    Fix a serious typo that causes all viewAs skill crashes.

--- src/package/wind.cpp
+++ src/package/wind.cpp
@@ -177,7 +177,7 @@ public:
 
     virtual Card *viewAs(const Card *originalCard) const{
         HuangtianCard *card = new HuangtianCard;
-        card->addSubcard(card);
+        card->addSubcard(originalCard);
 
         return card;
     }
@@ -1035,7 +1035,7 @@ public:
         if(ClientInstance->getStatus() == Client::Responsing){
             GuhuoCard *card = new GuhuoCard;
             card->setUserString(ClientInstance->getPattern());
-            card->addSubcard(card);
+            card->addSubcard(originalCard);
             return card;
         }
 
@@ -1043,7 +1043,7 @@ public:
         if(c){
             GuhuoCard *card = new GuhuoCard;
             card->setUserString(c->objectName());
-            card->addSubcard(card);
+            card->addSubcard(originalCard);
 
             return card;
         }else
