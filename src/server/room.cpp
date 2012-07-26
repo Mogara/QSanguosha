fadc372aeaa6b02c5935bdab7500e5199b5a03aa
Slob <xindonger@gmail.com>
2012-07-27 05:16:46 +0800

    fix goto_next button

--- src/server/room.cpp
+++ src/server/room.cpp
@@ -435,7 +435,7 @@ void Room::gameOver(const QString &winner){
             id.replace("_mini_","");
             int stage = Config.value("MiniSceneStage",1).toInt();
             int current = id.toInt();
-            if((stage == current) && stage<21)
+            if((stage == current) && stage<33)
             {
                 Config.setValue("MiniSceneStage",current+1);
                 id = QString::number(stage+1).rightJustified(2,'0');
