35fdfe2a73de5bb49acedadf6c34eca8a7ad8589
Paracel <paracel_007@sina.com.cn>
2012-08-27 01:42:08 +0800

    little adjustment to BGM Gan Ning according to the FAQ

--- src/package/bgm-package.cpp
+++ src/package/bgm-package.cpp
@@ -1051,6 +1051,10 @@ public:
         return to_select->isBlack();
     }
 
+    virtual bool isEnabledAtPlay(const Player *player) const{
+        return player->getPile("brocade").length() < 4;
+    }
+
     virtual const Card *viewAs(const Card *originalcard) const{
         YinlingCard *card = new YinlingCard;
         card->addSubcard(originalcard);
