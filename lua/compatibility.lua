c92b242a911e03c83d2dbfb173bddef9c37fc823
Paracek <paracel_007@sina.com.cn>
2013-01-22 13:12:41 +0800

    typo

--- lua/compatibility.lua
+++ lua/compatibility.lua
@@ -1,6 +1,6 @@
 -- compatibility
 
-sgs.Card_Skill = sgs.Card_Skill or sgs.Card_Typekill
+sgs.Card_Skill = sgs.Card_Skill or sgs.Card_TypeSkill
 sgs.Card_Basic = sgs.Card_Basic or sgs.Card_TypeBasic
 sgs.Card_Equip = sgs.Card_Equip or sgs.Card_TypeEquip
 sgs.Card_Trick = sgs.Card_Trick or sgs.Card_TypeTrick
