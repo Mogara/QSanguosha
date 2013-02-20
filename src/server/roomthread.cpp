4b10fe49a388efffb447bd5d6f7c5d206992fba0
Slob <BeginnerSlob@hotmail.com>
2013-02-20 21:12:48 +0800

    it's amusing fix mini_scenarios

--- src/server/roomthread.cpp
+++ src/server/roomthread.cpp
@@ -481,7 +481,7 @@ void RoomThread::run(){
 
 static bool CompareByPriority(const TriggerSkill *a, const TriggerSkill *b){
     if(a->getPriority() == b->getPriority())
-        return b->inherits("WeaponSkill") || b->inherits("ArmorSkill");
+        return b->inherits("WeaponSkill") || b->inherits("ArmorSkill") || b->inherits("GameRule");
     return a->getPriority() > b->getPriority();
 }
 
