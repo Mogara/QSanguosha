451d8e02b2e76458f3ac6f24c8b7da0caf309811
haveatry823 <401068081@qq.com>
2013-03-07 14:32:08 +0800

    场景模式小调整， 允许同时启用多个场景。

--- src/scenario/scenerule.cpp
+++ src/scenario/scenerule.cpp
@@ -9,11 +9,16 @@ int SceneRule::getPriority() const{
     return -2;
 }
 
-bool SceneRule::trigger(TriggerEvent triggerEvent, Room* room, ServerPlayer *player, QVariant &data) const {
-    if (!player && triggerEvent == GameStart  && Sanguosha->getSkill("#scenerule")){
-        foreach(ServerPlayer *p, room->getPlayers()){
-            room->acquireSkill(p, "#scenerule");
-        }        
+bool SceneRule::trigger(TriggerEvent triggerEvent, Room* room, ServerPlayer *player, QVariant &data) const {    
+    QStringList extensions = Sanguosha->getExtensions();
+    if (!player && triggerEvent == GameStart){
+        foreach(QString extension, extensions){
+            QString skill = QString("#%1").arg(extension);
+            if (extension.startsWith("scene") && Sanguosha->getSkill(skill)){
+                foreach(ServerPlayer *p, room->getPlayers())
+                    room->acquireSkill(p, skill);                
+            }
+        }
     }
     
     return GameRule::trigger(triggerEvent, room, player, data);
