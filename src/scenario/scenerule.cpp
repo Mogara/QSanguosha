b7d7d37888ddc8b9bfa706296939cc9706b17d5a
haveatry823 <401068081@qq.com>
2013-03-08 21:43:48 +0800

    fixed a bug in scenerule

--- src/scenario/scenerule.cpp
+++ src/scenario/scenerule.cpp
@@ -1,3 +1,4 @@
+#include "settings.h"
 #include "engine.h"
 #include "scenerule.h"
 
@@ -11,8 +12,13 @@ int SceneRule::getPriority() const{
 
 bool SceneRule::trigger(TriggerEvent triggerEvent, Room* room, ServerPlayer *player, QVariant &data) const {    
     QStringList extensions = Sanguosha->getExtensions();
+    QSet<QString> ban_packages = Config.BanPackages.toSet();
+    
     if (!player && triggerEvent == GameStart){
         foreach(QString extension, extensions){
+            bool forbid_package = Config.value("ForbidPackages").toStringList().contains(extension);
+            if (ban_packages.contains(extension) || forbid_package) continue;
+
             QString skill = QString("#%1").arg(extension);
             if (extension.startsWith("scene") && Sanguosha->getSkill(skill)){
                 foreach(ServerPlayer *p, room->getPlayers())
