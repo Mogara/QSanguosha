cf2e46a9a0282be550d661dc215996c960ab8ec2
Pan, Shi Zhu <pan.shizhu@gmail.com>
2013-07-04 16:09:40 +0800

    fix compiler error in debug mode

--- sanguosha.ts
+++ sanguosha.ts
@@ -3205,7 +3205,7 @@
  %1</translation>
     </message>
     <message>
-        <location filename="src/server/room.cpp" line="5435"/>
+        <location filename="src/server/room.cpp" line="5440"/>
         <source>&lt;font color=#EEB422&gt;The network delay of player &lt;b&gt;%1&lt;/b&gt; is %2 milliseconds.&lt;/font&gt;</source>
         <translation>&lt;font color=#EEB422&gt;玩家 &lt;b&gt;%1&lt;/b&gt; 的网络延迟为 %2 毫秒&lt;/font&gt;</translation>
     </message>
