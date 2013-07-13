df5fd3a4e21b4a7b65865f07eec958a4877ab333
Ubun <ubun@foxmail.com>
2013-07-13 15:41:14 +0800

    Add lords' win effect

--- swig/CXX-Creator.bat
+++ swig/CXX-Creator.bat
@@ -1,5 +1,5 @@
 @echo off
 @echo Creating sanguosha_wrap.cxx, please wait...
 
-f:\Qt\swigwin-1.3.39\swig -c++ -lua %1
-e:\Qt\swigwin-1.3.39\swig -c++ -lua %1
+f:\Qt\swigwin-2.0.10\swig -c++ -lua %1
+E:\QT\swigwin-2.0.10\swig -c++ -lua %1
