112fb006c3704cd14825726e3e126637bbe57e3c
Moligaloo <moligaloo@gmail.com>
2013-07-21 00:14:16 +0800

    Delete unused translation files

--- lang/zh-CN/CoupleScenario.lua
+++
@@ -1,9 +0,0 @@
--- translation for CoupleScenario
-
-return {
-	["couple"] = "夫妻协战",
-	["Couple"] = "夫妻",
-	["reselect"] = "重选",
-	["#Marry"] = "恭喜 %from 与 %to 喜结连理！",
-	["#Divorse"] = "负心的 %from 抛弃了可怜的 %to",
-}
