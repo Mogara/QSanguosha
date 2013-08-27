8d6176b6ba3ce5a1d2bc40f5a3abbd9a26a69c4d
Yanguam Sliagim <yanguam@foxmail.com>
2013-08-27 13:54:43 +0800

    adjust AI of Jilei

--- lua/ai/sp-ai.lua
+++ lua/ai/sp-ai.lua
@@ -137,8 +137,8 @@ end
 
 sgs.ai_skill_choice.jilei = function(self, choices)
 	local tmptrick = sgs.Sanguosha:cloneCard("ex_nihilo", sgs.Card_NoSuit, 0)
-	if (self:isEquip("Crossbow",self.jilei_source) and self.jilei_source:inMyAttackRange(self.player)) or
-		self.jilei_source:isCardLimited(tmptrick, sgs.Card_MethodUse, true) then
+	if (self:hasCrossbowEffect(self.jilei_source) and self.jilei_source:inMyAttackRange(self.player))
+		or self.jilei_source:isCardLimited(tmptrick, sgs.Card_MethodUse, true) then
 		return "BasicCard"
 	else
 		return "TrickCard"
