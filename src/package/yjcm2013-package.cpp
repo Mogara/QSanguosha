3ae724efc942b60d5620bb63b37fcf41a7b07919
Fsu0413 <Fsu0413@gmail.com>
2013-09-15 09:39:01 +0800

    edit qiuyuan according to the change of this skill

--- src/package/yjcm2013-package.cpp
+++ src/package/yjcm2013-package.cpp
@@ -1139,7 +1139,7 @@ public:
                         use.to.append(target);
                         room->sortByActionOrder(use.to);
                         data = QVariant::fromValue(use);
-                        room->getThread()->trigger(TargetConfirming, room, target, data);
+                        //room->getThread()->trigger(TargetConfirming, room, target, data);
                     }
                 }
             }
