866f3fee871b73ae4660dca56042f24c5a1b836c
Yanguam Sliagim <yanguam@foxmail.com>
2013-11-10 18:32:45 +0800

    Remove ShensuCard

--- src/package/wind.h
+++ src/package/wind.h
@@ -19,16 +19,6 @@ public:
     virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
 };
 
-class ShensuCard: public SkillCard {
-    Q_OBJECT
-
-public:
-    Q_INVOKABLE ShensuCard();
-
-    virtual bool targetFilter(const QList<const Player *> &targets, const Player *to_select, const Player *Self) const;
-    virtual void use(Room *room, ServerPlayer *source, QList<ServerPlayer *> &targets) const;
-};
-
 class TianxiangCard: public SkillCard {
     Q_OBJECT
 
