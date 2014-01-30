f9bc59341e43e871b6eb06795d5782b93201dd71
omnisreen <422419946@qq.com>
2014-01-30 12:08:47 +0800

    fix peacespell

--- src/package/momentum.cpp
+++ src/package/momentum.cpp
@@ -1318,7 +1318,7 @@ public:
         if (ps_owner == NULL)
             return 0;
 
-        if (target->isFriendWith(ps_owner) || target == ps_owner)
+        if (target->isFriendWith(ps_owner))
             return ps_owner->getPlayerNumWithSameKingdom();
 
         return 0;
