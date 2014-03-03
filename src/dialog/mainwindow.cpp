5068d638b528c991bb0f348290f2b89548bed302
Fsu0413 <Fsu0413@gmail.com>
2014-03-03 10:31:17 +0800

    ........

--- src/dialog/mainwindow.cpp
+++ src/dialog/mainwindow.cpp
@@ -494,7 +494,7 @@ void MainWindow::on_actionShow_Hide_Menu_triggered()
 void MainWindow::on_actionMinimize_to_system_tray_triggered()
 {
     if (systray == NULL) {
-        QIcon icon("image/system/magatamas/5.png");
+        QIcon icon("image/system/magatamas/3.png");
         systray = new QSystemTrayIcon(icon, this);
 
         QAction *appear = new QAction(tr("Show main window"), this);
