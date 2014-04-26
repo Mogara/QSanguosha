667157bde598b6fb405b036f7d78fcc9e938f8c0
Fsu0413 <Fsu0413@gmail.com>
2014-04-26 22:09:30 +0800

    rebuild QSanSMTPClient.exe

--- QSanSMTPClient/QSanSMTPClient/QSanSMTPClient.ts
+++ QSanSMTPClient/QSanSMTPClient/QSanSMTPClient.ts
@@ -4,7 +4,7 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="113"/>
         <source>The Program Crashed</source>
         <translation>游戏崩溃</translation>
     </message>
@@ -15,7 +15,7 @@
         <translation>请稍候...</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="113"/>
         <source>I regret to tell you that the program crashed just now. Fortunately, We have generated an error report successfully. 
  The problem may be solved if you click &quot;Yes&quot; to upload the report to us. It may take you a few minutes.</source>
         <translation>很遗憾，程序崩溃了。我们已为你生成了一份错误报告。 
@@ -23,22 +23,22 @@
 点击“是”--将报告传给开发团队以协助我们解决问题，这可能需要几分钟的时间。</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="120"/>
+        <location filename="mainwindow.cpp" line="121"/>
         <source>Error!</source>
         <translation>网络错误！</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="120"/>
+        <location filename="mainwindow.cpp" line="121"/>
         <source>Failed to connect to the server</source>
         <translation>服务器连接失败</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="151"/>
+        <location filename="mainwindow.cpp" line="152"/>
         <source>Congratulation</source>
         <translation>上传成功</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="151"/>
+        <location filename="mainwindow.cpp" line="152"/>
         <source>The report has been uploaded successfully. We will parse the report and try to fix the crash as soon as possible. Thank you very much. 
 QSanguosha-Hegemony Team</source>
         <translation>报告已成功上传至服务器，我们将尽快组织错误分析并尽最大努力修复。谢谢您的配合。
@@ -46,12 +46,12 @@ QSanguosha-Hegemony Team</source>
 太阳神三国杀·国战开发团队</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="153"/>
+        <location filename="mainwindow.cpp" line="154"/>
         <source>Hint</source>
         <translation>提示</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="153"/>
+        <location filename="mainwindow.cpp" line="154"/>
         <source>You can upload the report to a cloud disk and inform us later on. The file name is %1. You could find it in the folder &quot;dmp&quot;. Thanks.  
 QSanguosha-Hegemony Team</source>
         <translation>你可以稍后将错误报告上传至云盘并联系我们获取。错误报告 %1 位于 &quot;dmp&quot; 文件夹下，谢谢。
