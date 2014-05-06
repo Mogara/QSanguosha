6bafe58a4cfb666db7d4bceb82816728ef7cf2d1
Fsu0413 <Fsu0413@gmail.com>
2014-05-06 22:00:30 +0800

    change the password in QSanSMTPClient
    because I was annoyed by an anonymous crash report of unknown version

--- QSanSMTPClient/QSanSMTPClient/mainwindow.cpp
+++ QSanSMTPClient/QSanSMTPClient/mainwindow.cpp
@@ -116,7 +116,7 @@ int MainWindow::askForUploading() {
 
         smtp.setName("QSGSH");
         smtp.setUser("QSGSH@qq.com");
-        smtp.setPassword("QSgsHegemony"); //since I use a temporary mobile phone number, this Password can finally put here
+        smtp.setPassword("QSgsHegemony072"); //since I use a temporary mobile phone number, this Password can finally put here
 
         if (!smtp.connectToHost()) {
             QMessageBox::warning(this, tr("Error!"), tr("Failed to connect to the server"), QMessageBox::Ok, QMessageBox::Ok);
