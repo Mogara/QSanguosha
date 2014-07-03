c8f5a2d02f894b9231ed3424412fef31d507aeb6
Fs <Fsu0413@vip.qq.com>
2014-07-03 20:34:37 +0800

    update fmod 2 4.44.38

--- include/fmod/fmod.h
+++ include/fmod/fmod.h
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00044437
+#define FMOD_VERSION    0x00044438
 
 /*
     Compiler specific settings.
