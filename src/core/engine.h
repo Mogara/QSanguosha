502a94227d072e18dc4aa19c2cb1121bfc35322c
YanGuam <yanguam@foxmail.com>
2014-07-08 15:10:16 +0800

    Move QSanVersionNumber from engine.h/.cpp

--- src/core/engine.h
+++ src/core/engine.h
@@ -46,39 +46,6 @@ class LuaArmor;
 
 struct lua_State;
 
-class QSanVersionNumber{
-public:
-    enum VersionType{
-        alpha,
-        beta,
-        offical,
-        other = -1
-    };
-
-    explicit QSanVersionNumber(const QString &ver_str);
-    QSanVersionNumber(int major, int minor, int sub, VersionType type = offical);
-
-    // Actually only these 2 operator overloads take effect here...
-    bool operator <(const QSanVersionNumber &arg2) const;
-    bool operator ==(const QSanVersionNumber &arg2) const;
-
-    // these 4 operator overloads are just for convenience...
-    bool operator >(const QSanVersionNumber &arg2) const;
-    bool operator !=(const QSanVersionNumber &arg2) const;
-    bool operator <=(const QSanVersionNumber &arg2) const;
-    bool operator >=(const QSanVersionNumber &arg2) const;
-
-    operator QString() const;
-    QString toString() const;
-    bool tryParse(const QString &ver_str);
-
-private:
-    int m_major;
-    int m_minor;
-    int m_sub;
-    VersionType m_type;
-};
-
 class Engine : public QObject {
     Q_OBJECT
 
