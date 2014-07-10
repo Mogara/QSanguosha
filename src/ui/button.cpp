ca22763fd3c1d5ba855723e266a0358fc7951c97
YanGuam <yanguam@foxmail.com>
2014-07-10 12:15:06 +0800

    Metro sytle button by Moligaloo

--- src/ui/button.cpp
+++ src/ui/button.cpp
@@ -19,28 +19,22 @@
     *********************************************************************/
 
 #include "button.h"
-#include "audio.h"
+#include "engine.h"
 
 #include <QPainter>
 #include <QGraphicsSceneMouseEvent>
-#include <QGraphicsRotation>
-#include <QPropertyAnimation>
 
 static QRectF ButtonRect(0, 0, 189, 46);
 
 Button::Button(const QString &label, qreal scale)
     : label(label), size(ButtonRect.size() * scale), mute(true), font(Config.SmallFont)
 {
-    title = QPixmap(size.toSize());
-    outimg = QImage(size.toSize(), QImage::Format_ARGB32);
     init();
 }
 
 Button::Button(const QString &label, const QSizeF &size)
     : label(label), size(size), mute(true), font(Config.SmallFont)
 {
-    title = QPixmap(size.toSize());
-    outimg = QImage(size.toSize(), QImage::Format_ARGB32);
     init();
 }
 
@@ -49,67 +43,6 @@ void Button::init() {
 
     setAcceptHoverEvents(true);
     setAcceptedMouseButtons(Qt::LeftButton);
-
-    title.fill(QColor(0, 0, 0, 0));
-    QPainter pt(&title);
-    pt.setFont(font);
-    pt.setPen(Config.TextEditColor);
-    pt.setRenderHint(QPainter::TextAntialiasing);
-    pt.drawText(boundingRect(), Qt::AlignCenter, label);
-
-    title_item = new QGraphicsPixmapItem(this);
-    title_item->setPixmap(title);
-    title_item->show();
-
-    de = new QGraphicsDropShadowEffect(this);
-    de->setOffset(0);
-    de->setBlurRadius(12);
-    de->setColor(QColor(255, 165, 0));
-
-    title_item->setGraphicsEffect(de);
-
-    QImage bgimg("image/system/button/button.png");
-
-    qreal pad = 10;
-
-    int w = bgimg.width();
-    int h = bgimg.height();
-
-    int tw = outimg.width();
-    int th = outimg.height();
-
-    qreal xc = (w - 2 * pad) / (tw - 2 * pad);
-    qreal yc = (h - 2 * pad) / (th - 2 * pad);
-
-    for (int i = 0; i < tw; i++) {
-        for (int j = 0; j < th; j++) {
-            int x = i;
-            int y = j;
-
-            if (x >= pad && x <= (tw - pad))
-                x = pad + (x - pad) * xc;
-            else if (x >= (tw - pad))
-                x = w - (tw - x);
-
-            if (y >= pad && y <= (th - pad))
-                y = pad + (y - pad) * yc;
-            else if (y >= (th - pad))
-                y = h - (th - y);
-
-
-            QRgb rgb = bgimg.pixel(x, y);
-            outimg.setPixel(i, j, rgb);
-        }
-    }
-
-    effect = new QGraphicsDropShadowEffect(this);
-    effect->setBlurRadius(5);
-    effect->setOffset(this->boundingRect().height() / 7.0);
-    effect->setColor(QColor(0, 0, 0, 200));
-    this->setGraphicsEffect(effect);
-
-    glow = 0;
-    timer_id = 0;
 }
 
 void Button::setMute(bool mute) {
@@ -118,23 +51,10 @@ void Button::setMute(bool mute) {
 
 void Button::setFont(const QFont &font) {
     this->font = font;
-
-    title.fill(QColor(0, 0, 0, 0));
-    QPainter pt(&title);
-    pt.setFont(font);
-    pt.setPen(Config.TextEditColor);
-    pt.setRenderHint(QPainter::TextAntialiasing);
-    pt.drawText(boundingRect(), Qt::AlignCenter, label);
-
-    title_item->setPixmap(title);
 }
-
-#include "engine.h"
 
 void Button::hoverEnterEvent(QGraphicsSceneHoverEvent *) {
     setFocus(Qt::MouseFocusReason);
-    if (!mute) Sanguosha->playSystemAudioEffect("button-hover");
-    if (!timer_id) timer_id = QObject::startTimer(40);
 }
 
 void Button::mousePressEvent(QGraphicsSceneMouseEvent *event) {
@@ -150,24 +70,37 @@ QRectF Button::boundingRect() const{
     return QRectF(QPointF(), size);
 }
 
-void Button::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) {
-    QRectF rect = boundingRect();
+static QColor ReverseColor(const QColor &color) {
+    int r = 0xFF - color.red();
+    int g = 0xFF - color.green();
+    int b = 0xFF - color.blue();
 
-    painter->drawImage(rect, outimg);
-    painter->fillRect(rect, QColor(255, 255, 255, glow * 10));
+    return QColor::fromRgb(r, g, b);
 }
 
-void Button::timerEvent(QTimerEvent *) {
-    update();
-    if (hasFocus()) {
-        if (glow < 5) glow++;
-    }
-    else {
-        if (glow > 0)
-            glow--;
-        else if (timer_id) {
-            QObject::killTimer(timer_id);
-            timer_id = 0;
-        }
-    }
+void Button::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) {
+	QRectF rect = boundingRect();
+
+	QColor textColor, edgeColor, boxColor;
+    textColor = edgeColor = Qt::white;
+    boxColor = Qt::black;
+
+	if (hasFocus()) {
+        textColor = ReverseColor(textColor);
+        boxColor = ReverseColor(boxColor);
+	}
+
+	boxColor.setAlphaF(0.8);
+
+	painter->fillRect(rect, boxColor);
+
+	QPen pen(edgeColor);
+	pen.setWidth(2);
+	painter->setPen(pen);
+	painter->drawRect(rect);
+
+    pen.setColor(textColor);
+    painter->setPen(pen);
+	painter->setFont(font);
+	painter->drawText(rect, Qt::AlignCenter, label);
 }
