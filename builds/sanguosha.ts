513c28fa049ada2c6fb31f4b8df1f73baa906db7
YanGuam <rara@qsanguosha.org>
2014-08-20 12:44:52 +0800

    Minor adjustment on translation by Qinyin

--- builds/sanguosha.ts
+++ builds/sanguosha.ts
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="zh_CN">
+<TS version="2.1" language="zh_CN">
 <context>
     <name>AboutUsDialog</name>
     <message>
@@ -4053,7 +4053,7 @@ P = 你自己, R = 你所在的房间</translation>
     <message>
         <location filename="../src/ui/startscene.cpp" line="44"/>
         <source>At 10:40 a.m., August 19, 2014, Moxuanyanyun, a developer of QSanguosha, passed away peacefully in Dalian Medical College. He was 18 and had struggled with leukemia for more than 4 years. May there is no pain in Heaven.</source>
-        <translation>公元2014年8月19日10时40分，神杀开发者墨宣砚韵于大连医学院平静离开，是时年仅18岁，与白血病斗争4年多。愿天堂没有病痛。</translation>
+        <translation>公元2014年8月19日10时40分，神杀开发者墨宣砚韵于大连医学院平静离开人世，是时年仅18岁，与白血病斗争4年多。愿天堂没有病痛。</translation>
     </message>
     <message>
         <location filename="../src/ui/startscene.cpp" line="230"/>
