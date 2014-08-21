90c95210f58f3fdb77a10021fc376a106eb2ee1b
Takashiro <takashiro@qq.com>
2014-08-21 23:01:09 +0800

    fix a warning from g++

--- src/core/player.cpp
+++ src/core/player.cpp
@@ -29,12 +29,12 @@ Player::Player(QObject *parent)
     : QObject(parent), owner(false), general(NULL), general2(NULL),
     m_gender(General::Sexless), hp(-1), max_hp(-1),
     role_shown(false), state("online"), seat(0), alive(true),
+    headSkinId(0), deputySkinId(0),
     actual_general1(NULL), actual_general2(NULL),
     general1_showed(false), general2_showed(false),
     phase(NotActive),
     weapon(NULL), armor(NULL), defensive_horse(NULL), offensive_horse(NULL), treasure(NULL),
-    face_up(true), chained(false), removed(false), scenario_role_shown(false),
-    headSkinId(0), deputySkinId(0)
+    face_up(true), chained(false), removed(false), scenario_role_shown(false)
 {
 }
 
