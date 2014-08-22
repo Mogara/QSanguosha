db8e23c157d5a1d8f6ffd023eb1d119eab32adaf
lrl026 <mystarcraft@163.com>
2014-08-23 00:04:19 +0800

    修正transfer ai

--- lua/ai/strategic_advantage-ai.lua
+++ lua/ai/strategic_advantage-ai.lua
@@ -86,8 +86,10 @@ transfer_skill.name = "transfer"
 table.insert(sgs.ai_skills, transfer_skill)
 transfer_skill.getTurnUseCard = function(self, inclusive)
 	if self.player:isKongcheng() then return end
-	if self:isWeak() and self:getOverflow() <= 0 then return end
-	if not self.player:hasShownOneGeneral() then return end
+	if not self.player:hasSkill("kongcheng") then
+		if self:isWeak() and self:getOverflow() <= 0 then return end
+		if not self.player:hasShownOneGeneral() then return end
+	end
 	return sgs.Card_Parse("@TransferCard=.")
 end
 
