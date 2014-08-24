98a6aa31adb544fb3bde9a26afd60a8015ce1015
YanGuam <rara@qsanguosha.org>
2014-08-25 03:23:37 +0800

    Just for test

---
+++ hero-skin/xiaoqiao/1/zh_CN.lua
@@ -0,0 +1,10 @@
+sgs.LoadTranslationTable {
+	["#1xiaoqiao"] = "fsu了不起",
+	["cv:1xiaoqiao"] = "fsu",
+	["illustrator:1xiaoqiao"] = "fsu",
+	
+	["$1tianxiang"] = "fsu了不起",
+	["$1hongyan"] = "fsu了不起",
+	
+	["~1xiaoqiao"] = "fsu~"
+}
