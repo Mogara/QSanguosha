ffa89e1f91b4dfc6db399cf57234e3370265ae11
YanGuam <rara@qsanguosha.org>
2014-08-30 21:31:10 +0800

    Temp translation
    仅供参考，产品请以实物为准

--- hero-skin/xiaoqiao/1/zh_CN.lua
+++ hero-skin/xiaoqiao/1/zh_CN.lua
@@ -1,10 +1,15 @@
+--Xiaoqiao G.SP 002 
 sgs.LoadSkinTransltionTable {
-	["#xiaoqiao"] = "fsu了不起",
-	["cv:xiaoqiao"] = "fsu",
-	["illustrator:xiaoqiao"] = "fsu",
+	["cv:xiaoqiao"] = "琴音",
+	["illustrator:xiaoqiao"] = "牧童的短笛",
 	
-	["$tianxiang"] = "fsu了不起",
-	["$hongyan"] = "fsu了不起",
+	["$tianxiang1"] = "惜玉怜香，倾心至上",
+	["$tianxiang2"] = "天香本是绝艳，奈何缘深情浅",
+	["$tianxiang3"] = "周郎，你必不负我", --弹周瑜
+
+	["$hongyan1"] = "红颜索清香，托付英雄将", --周瑜已亮
+	["$hongyan2"] = "一朝歌舞弄心弦，香花洒满人世间", --乐不思蜀、闪电改判亮将
+	["$hongyan3"] = "可敢为我，倾城一怒？",
 	
-	["~xiaoqiao"] = "fsu~"
+	["~xiaoqiao"] = "此间真情已断，有缘来世还牵……"
 }
---
+++ hero-skin/zhouyu/1/zh_CN.lua
@@ -0,0 +1,11 @@
+--Zhou Yu G.SP 001
+sgs.LoadSkinTransltionTable {
+	["cv:zhouyu"] = "fsu",
+	["illustrator:zhouyu"] = "牧童的短笛",
+	
+	["$yingzi"] = "萨比KK",
+
+	["$fanjian"] = "萨比穹妹",
+	
+	["~zhouyu"] = "⊙▂⊙啊啊啊啊啊啊啊啊"
+}
