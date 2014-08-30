e12b57eb21b679ad7c0fa130b41ed34150e7b7dc
YanGuam <rara@qsanguosha.org>
2014-08-30 23:42:11 +0800

    Update line translation of G.SP Zhou Yu

--- hero-skin/zhouyu/1/zh_CN.lua
+++ hero-skin/zhouyu/1/zh_CN.lua
@@ -1,11 +1,14 @@
 --Zhou Yu G.SP 001
 sgs.LoadSkinTransltionTable {
-	["cv:zhouyu"] = "fsu",
+	["cv:zhouyu"] = "孝直",
 	["illustrator:zhouyu"] = "牧童的短笛",
 	
-	["$yingzi"] = "萨比KK",
+	["$yingzi1"] = "凭七尺之躯，当有不世之为",
+	["$yingzi2"] = "生擒曹操，宜在今日！",
 
-	["$fanjian"] = "萨比穹妹",
+	["$fanjian1"] = "此计中之计耳",
+	["$fanjian2"] = "此计若成，曹军必溃。",
+	["$fanjian3"] = "咎由自取，休怪我不留情面。", --猜错
 	
-	["~zhouyu"] = "⊙▂⊙啊啊啊啊啊啊啊啊"
+	["~zhouyu"] = "子敬，这副重担，只有托付给你了。"
 }
