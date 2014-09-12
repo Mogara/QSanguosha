30570e537bf4c8ef2e98ab04192fddb1cb4a3963
Takashiro <takashiro@qq.com>
2014-09-12 16:06:07 +0800

    add new command to enter lobby

--- src/core/protocol.h
+++ src/core/protocol.h
@@ -155,7 +155,8 @@ namespace QSanProtocol {
         S_COMMAND_DISABLE_SHOW,
         S_COMMAND_TRIGGER_ORDER,
         S_COMMAND_MIRROR_GUANXING_STEP,
-        S_COMMAND_CHANGE_SKIN
+        S_COMMAND_CHANGE_SKIN,
+        S_COMMAND_ENTER_LOBBY
     };
 
     enum GameEventType {
