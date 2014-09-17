94d7c03bb436ca11bc1cbe3973c3b395c9eeae80
Takashiro <takashiro@qq.com>
2014-09-17 13:56:04 +0800

    fix a typo

--- src/package/standard-tricks.cpp
+++ src/package/standard-tricks.cpp
@@ -113,7 +113,7 @@ void SavageAssault::onEffect(const CardEffectStruct &effect) const{
         Card::MethodResponse,
         effect.from->isAlive() ? effect.from : NULL);
     if (slash) {
-        if (slash->getSkillName() == "spear") room->setEmotion(effect.to, "weapon/spear");
+        if (slash->getSkillName() == "Spear") room->setEmotion(effect.to, "weapon/spear");
         room->setEmotion(effect.to, "killer");
     }
     else{
