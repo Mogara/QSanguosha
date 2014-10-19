19a8dfbf1fec6588de806c0c32bab223a923410b
Fs <Fsu0413@QSanguosha.org>
2014-10-20 02:31:49 +0800

    fix QSanVersionNumber

--- src/core/version.cpp
+++ src/core/version.cpp
@@ -34,22 +34,22 @@ QSanVersionNumber::QSanVersionNumber(int major, int minor, int sub, VersionType 
 }
 
 bool QSanVersionNumber::tryParse(const QString &str){
-    QRegExp regexp("(\\d+)\\.(\\d+)\\.(\\d+)\\-([a-z]+)(\\d+)?");
+    QRegExp regexp("(\\d+)\\.(\\d+)\\.(\\d+)(\\-([a-z]+)(\\d+))?");
     if (regexp.exactMatch(str)){
         QStringList l = regexp.capturedTexts();
         m_major = l[1].toInt();
         m_minor = l[2].toInt();
         m_sub = l[3].toInt();
-        if (l[4] == "alpha")
+        if (l[5] == "alpha")
             m_type = alpha;
-        else if (l[4] == "beta")
+        else if (l[5] == "beta")
             m_type = beta;
-        else if (l[4] == "offical")
+        else if (l[5] == "offical")
             m_type = offical;
         else
             m_type = other;
-        if (l.length() > 4)
-            m_step = l[5].toInt();
+        if (l.length() > 5)
+            m_step = l[6].toInt();
         else
             m_step = 0;
 
@@ -81,7 +81,7 @@ QString QSanVersionNumber::toString() const{
     }
     str = str.arg(m_major).arg(m_minor).arg(m_sub).arg(type_str);
     if (m_step != 0)
-        str.append(m_step);
+        str.append(QString::number(m_step));
 
     return str;
 }
