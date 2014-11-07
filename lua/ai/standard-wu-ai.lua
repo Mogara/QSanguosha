7d72fd8e4d9a7688dd27a30487287747e7ff8813
zyun7799 <zyun7799@163.com>
2014-11-07 20:12:06 +0800

    fix yingzi+haoshi

--- lua/ai/standard-wu-ai.lua
+++ lua/ai/standard-wu-ai.lua
@@ -389,7 +389,7 @@ sgs.ai_skill_invoke.yingzi = function(self, data)
 					target = p
 				end
 			end
-			if target and self:isFriend(target) then return true end
+			if target and not self:isFriend(target) and num + count > 6 then return false
 		end
 	end
 	return true
