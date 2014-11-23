a2cc3a665d3dccfb2cf4a954782d57c16abfc5d6
YanGuam <rara@qsanguosha.org>
2014-11-23 12:30:25 +0800

    Refine skill description

--- src/core/skill.cpp
+++ src/core/skill.cpp
@@ -55,10 +55,10 @@ bool Skill::isAttachedLordSkill() const{
     return attached_lord_skill;
 }
 
-QString Skill::getDescription(bool inToolTip) const{
-    QString desc = "<br/>";
+QString Skill::getDescription(bool inToolTip) const {
+    QString desc;
     if (!canPreshow())
-        desc.prepend(QString("<font color=gray>(%1)</font>").arg(tr("this skill cannot preshow")));
+        desc.prepend(QString("<font color=gray>(%1)</font><br/>").arg(tr("this skill cannot preshow")));
 
     QString skill_name = objectName();
 
@@ -69,6 +69,7 @@ QString Skill::getDescription(bool inToolTip) const{
     QString des_src = Sanguosha->translate(":" + skill_name);
     if (des_src == ":" + skill_name)
         return desc;
+
     foreach(QString skill_type, Sanguosha->getSkillColorMap().keys()) {
         QString to_replace = Sanguosha->translate(skill_type);
         if (to_replace == skill_type) continue;
