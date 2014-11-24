f67cbfdee5eedddee0ed38101443f3d0b4fe4a4f
BeginnerSlob <BeginnerSlob@gmail.com>
2014-11-24 20:46:43 +0800

    修复两个BUG 1、无法对野心家使用方天杀 2、暗将丁奉发动短兵不会自动亮将

--- src/package/standard-basics.cpp
+++ src/package/standard-basics.cpp
@@ -198,7 +198,7 @@ void Slash::onUse(Room *room, const CardUseStruct &card_use) const{
         room->setPlayerFlag(player, "-slashNoDistanceLimit");
     if (player->hasFlag("slashDisableExtraTarget"))
         room->setPlayerFlag(player, "-slashDisableExtraTarget");
-
+    // for Paoxiao
     if (player->getPhase() == Player::Play && player->hasFlag("Global_MoreSlashInOneTurn")) {
         if (player->hasSkill("paoxiao")) {
             if (!player->hasShownSkill("paoxiao"))
@@ -208,14 +208,17 @@ void Slash::onUse(Room *room, const CardUseStruct &card_use) const{
             room->notifySkillInvoked(player, "paoxiao");
         }
     }
+    // for Tianyi
     if ((use.to.size() > 1 + player->getMark("halberd_count")
         || (player->hasFlag("Global_MoreSlashInOneTurn") && player->getSlashCount() == 2))
         && player->hasFlag("TianyiSuccess") && player->getPhase() == Player::Play) {
         if (player->hasFlag("Global_MoreSlashInOneTurn")) // Tianyi just let player could use one more Slash
             room->setPlayerFlag(player, "-Global_MoreSlashInOneTurn");
         room->broadcastSkillInvoke("tianyi", 1, player);
-    } else if (use.to.size() > 1 + Sanguosha->correctCardTarget(TargetModSkill::ExtraTarget, player, this)
-               && player->hasSkill("duanbing")) {
+    }
+    // for Duanbing
+    if (use.to.size() > 1 + Sanguosha->correctCardTarget(TargetModSkill::ExtraTarget, player, this)
+        && player->hasSkill("duanbing")) {
         if (!player->hasShownSkill("duanbing"))
             player->showGeneral(player->inHeadSkills("duanbing"));
         room->broadcastSkillInvoke("duanbing", player);
@@ -334,12 +337,14 @@ bool Slash::targetFilter(const QList<const Player *> &targets, const Player *to_
     if (Self->hasFlag("HalberdSlashFilter")) {
         QSet<QString> kingdoms;
         foreach (const Player *p, targets) {
-            if (!p->hasShownOneGeneral())
+            if (!p->hasShownOneGeneral() || p->getRole() == "careerist")
                 continue;
             kingdoms << p->getKingdom();
         }
         if (to_select->getMark("Equips_of_Others_Nullified_to_You") > 0)
             return false;
+        if (to_select->hasShownOneGeneral() && to_select->getRole() == "careerist") // careerist!
+            return true;
         if (to_select->hasShownOneGeneral() && kingdoms.contains(to_select->getKingdom()))
             return false;
     } else if (targets.length() >= slash_targets) {
