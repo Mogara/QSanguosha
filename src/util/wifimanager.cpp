704e408a2f5aeee5569932ca7d910bc4bf6437f6
Takashiro <takashiro@qq.com>
2015-01-06 14:15:59 +0800

    [0.1.0][enhancement][0][实现探测Wifi热点功能(Android)][network]

--- src/util/wifimanager.cpp
+++ src/util/wifimanager.cpp
@@ -1,5 +1,7 @@
 #include "wifimanager.h"
 
+const char *WifiManager::SSID_PREFIX = "QSanguosha-";
+
 #ifdef Q_OS_ANDROID
 #include <QtAndroid>
 #include <QAndroidJniObject>
@@ -9,8 +11,8 @@ static QAndroidJniObject activity = QtAndroid::androidActivity();
 static QAndroidJniObject manager = activity.callObjectMethod("getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;", QAndroidJniObject::fromString("wifi").object<jstring>());
 #endif
 
-WifiManager::WifiManager(const QString &serverName)
-    :mServerName(serverName)
+WifiManager::WifiManager(const QString &deviceName)
+    :mDeviceName(deviceName)
 {
 }
 
@@ -19,7 +21,7 @@ bool WifiManager::enableHotspot()
 #ifdef Q_OS_ANDROID
     setWifiEnabled(false);
 
-    QAndroidJniObject ssid = QAndroidJniObject::fromString(QString("QSanguoshaServer-%1").arg(mServerName));
+    QAndroidJniObject ssid = QAndroidJniObject::fromString(QString("%1%2").arg(SSID_PREFIX).arg(mDeviceName));
     QAndroidJniObject key = QAndroidJniObject::fromString("QSanguosha");
 
     QAndroidJniObject config("android/net/wifi/WifiConfiguration");
@@ -33,16 +35,51 @@ bool WifiManager::enableHotspot()
 
 bool WifiManager::disableHotspot()
 {
+#ifdef Q_OS_ANDROID
+    return manager.callMethod<jboolean>("setWifiApEnabled", "(Landroid/net/wifi/WifiConfiguration;Z)Z", NULL, false);
+#else
+    return false;
+#endif
+}
+
+bool WifiManager::isWifiEnabled() const
+{
+#ifdef Q_OS_ANDROID
+    return manager.callMethod<jboolean>("isWifiEnabled");
+#else
+    return false;
+#endif
 }
 
 bool WifiManager::setWifiEnabled(bool enabled)
 {
+#ifdef Q_OS_ANDROID
     return manager.callMethod<jboolean>("setWifiEnabled", "(Z)Z", enabled);
+#else
+    return false;
+#endif
 }
 
 QStringList WifiManager::detect()
 {
-    return QStringList();
+    if(!isWifiEnabled())
+        setWifiEnabled(true);
+
+    QStringList hotspots;
+#ifdef Q_OS_ANDROID
+    manager.callMethod<jboolean>("startScan");
+    QAndroidJniObject results = manager.callObjectMethod("getScanResults", "()Ljava/util/List;");
+    int length = results.callMethod<jint>("size");
+    for (int i = 0; i < length; i++) {
+        QAndroidJniObject result = results.callObjectMethod("get", "(I)Ljava/lang/Object;", i);
+        QString ssid = result.getObjectField("SSID", "Ljava/lang/String;").toString();
+        if (ssid.startsWith(SSID_PREFIX)) {
+            ssid.remove(0, strlen(SSID_PREFIX));
+            hotspots << ssid;
+        }
+    }
+#endif
+    return hotspots;
 }
 
 bool WifiManager::connect(const QString &ssid)
--- src/util/wifimanager.h
+++ src/util/wifimanager.h
@@ -6,18 +6,20 @@
 class WifiManager
 {
 public:
-    WifiManager(const QString &serverName);
+    WifiManager(const QString &deviceName);
 
     bool enableHotspot();
     bool disableHotspot();
 
+    bool isWifiEnabled() const;
     bool setWifiEnabled(bool enabled);
 
     QStringList detect();
     bool connect(const QString &ssid);
 
 private:
-    QString mServerName;
+    QString mDeviceName;
+    static const char *SSID_PREFIX;
 };
 
 #endif // WIFIMANAGER_H
