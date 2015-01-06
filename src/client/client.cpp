35f677387b355388d0fc2d8722f30aa1ace795f4
Fs <Fsu0413@QSanguosha.org>
2015-01-06 23:00:21 +0800

    [0.1.0][enhancement][0][修复chatbox的playername][ui]

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -2010,8 +2010,7 @@ void Client::speak(const QVariant &speak) {
 
     QString title;
     if (from) {
-        title = from->getGeneralName();
-        title = Sanguosha->translate(title);
+        title = getPlayerName(from->objectName());
         title.append(QString("(%1)").arg(from->screenName()));
     } else {
         title = who;
